158a3031b9e7e2d6f8eba5c3fc64854ac49c2bfa
The_Gost_sniper <54660359+TheGostsniperfr@users.noreply.github.com>
2024-05-06 06:35:29 +0200

    ✅ Add Hilt db module

--- app/src/main/java/fr/thegostsniperfr/todoapp/data/ToDoDatabase.kt
+++ app/src/main/java/fr/thegostsniperfr/todoapp/data/ToDoDatabase.kt
@@ -6,5 +6,5 @@ import fr.thegostsniperfr.todoapp.data.models.ToDoTask
 
 @Database(entities = [ToDoTask::class], version = 1, exportSchema = false)
 abstract class ToDoDatabase: RoomDatabase() {
-    abstract fun ToDoDoa(): ToDoDoa
+    abstract fun toDoDoa(): ToDoDoa
 }
---
+++ app/src/main/java/fr/thegostsniperfr/todoapp/di/DatabaseModule.kt
@@ -0,0 +1,34 @@
+package fr.thegostsniperfr.todoapp.di
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import fr.thegostsniperfr.todoapp.data.ToDoDatabase
+import fr.thegostsniperfr.todoapp.utils.Constants.DATABASE_NAME
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        ToDoDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(
+        database: ToDoDatabase
+    ) = database.toDoDoa()
+}
